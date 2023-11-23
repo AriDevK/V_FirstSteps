19e7a3b82a9f3ba3f61939bc42dc36b070db0d49
AriDevK <AriadneFave@hotmail.com>
2023-11-22 23:03:25 -0600

    Playing with maps and first for loop

--- main.v
+++ main.v
@@ -1,30 +1,19 @@
 module main
 
 fn main() {
-	mut data := [1, 2, 3, 4]
+	// very similar as go
+	mut data := map[string]string{} // idk why should I add the {} at the end (?)
+
+	data['name'] = 'Ariadne'
+	data['gender'] = 'female'
+
+	print('The map/dictionary data is: ')
 	println(data)
 
-	// adding data into array (this add on the first parameter index)
-	data.insert(2, [5, 6, 7])
-	println(data)
+	println('===================================')
 
-	// there is not any append function, so the way is like this
-	data = [1, 2, 3, 4] // restarting the data just for fun :D
-	data.insert(data.len, [5, 6, 7])
-	println(data)
-
-	// the other way to append data is using this operator <<
-	data << 8
-	println(data)
-
-	// we can also filter data from the array using filter like JS or the Where of .NET
-	// |--- yeah var names use snake_case (not really fan being honest)
-	// v
-	new_data := data.filter(it % 2 == 0)
-	println(new_data)
-
-	// and also (like JS) we have the map function in order to apply a method to each
-	// item of an array ("aggregate" on .NET)
-	mapped_data := data.map(it * 2)
-	println(mapped_data)
+	println('Looping map values with a for:')
+	for k, v in data {
+		println(k + ' : ' + v)
+	}
 }
