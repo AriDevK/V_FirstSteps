3d00e308a53259643cb9d63739bae294c5e7553d
AriDevK <AriadneFave@hotmail.com>
2023-11-22 22:48:41 -0600

    Playing with string methods

--- main.v
+++ main.v
@@ -1,9 +1,28 @@
 module main
 
 fn main() {
-	mut msg := 'Hello World!'
+	// print simple message
+	mut msg := 'Hello'
 	println(msg)
 
-	msg = 'This is a new message'
+	// concat text to a message
+	msg += ' web friends'
 	println(msg)
+
+	// check if string contains text
+	println('the message contains web? ' + msg.contains('web').str())
+
+	// capitalize the message
+	println('this message is uppercase ' + msg.to_upper())
+
+	// get string length
+	println('this is the message length ' + msg.len.str())
+
+	// replacing web to world
+	msg = msg.replace('web', 'world')
+	println('the message has changed with ' + msg)
+
+	// splitting the message
+	print('this are the message words ')
+	println(msg.split(' '))
 }
